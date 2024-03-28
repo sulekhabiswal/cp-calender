c53fe6ea860c8b5d8c1e54cadb0f6329dbf3e2fe
sulekhabiswal <103110686+sulekhabiswal@users.noreply.github.com>
2024-03-28 21:46:29 +0530

    Add files via upload

---
+++ cp_calander/js/background.js
@@ -0,0 +1,15 @@
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log(alarm.name);
+    chrome.tabs.create({
+        active: true,
+        url: alarm.name
+    }, null);
+});
+
+chrome.notifications.onClicked.addListener(() => {
+    console.log("Happen");
+});
+
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command "${command}" called`);
+});
